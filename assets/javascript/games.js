bb9bdd4e14d511463e71cce5f62cc13c6cb76dbd
Alexandra M <alexandramatveyeva@yahoo.com>
2018-06-05 19:07:42 -0700

    new README, key evnt and comp Options

---
+++ assets/javascript/games.js
@@ -0,0 +1,18 @@
+//press the key to get started
+
+alert("Hello");
+function hitAnyKey (event) {
+	alert("Hello");
+	//var userGuess = event.key;
+	//document.querySelector(#game).innerText=
+}
+
+
+document.onKey=hitAnyKey;
+
+
+//the computer make choice of a word
+//display it like _ _ _ _
+//user make a choice of a letter
+//compare the choice and determine if the letter is present in the word
+//display the result
