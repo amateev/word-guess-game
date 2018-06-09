5d10d1ba9a74a63762757a229d427f3110d8f529
Alexandra M <alexandramatveyeva@yahoo.com>
2018-06-08 19:00:31 -0700

    setting up the environment

--- assets/javascript/games.js
+++ assets/javascript/games.js
@@ -1,18 +1,92 @@
-//press the key to get started
+// create an array of words
+// choose word randomly
+// create underscores for a random length of word
+// get the playes guess
+// compare the guesses to computer
 
-alert("Hello");
-function hitAnyKey (event) {
-	alert("Hello");
-	//var userGuess = event.key;
-	//document.querySelector(#game).innerText=
+
+
+
+	var words = ["dog", "cat", "bear", "cow"];
+		
+	var count = 10;	
+	var wins;
+	
+	var wordArray = [];
+	var str;
+
+
+	function startGame(event) {
+
+//trigger the game
+		
+		var userChoice = event.key;
+		document.querySelector('#userChoice').innerText = userChoice;
+
+		var wins = 0;
+		document.querySelector('#wins').innerText = wins;
+		
+		computerOption = words[Math.floor(Math.random()*words.length)];
+		console.log(computerOption);
+
+		for(var i=0; i<computerOption.length; i++) {
+
+			wordArray[i] = "_";
+			str = wordArray.join(" ");
+			document.querySelector('#word').innerHTML = str;
+		}
+
+if(userChoice.indexOf(computerOption) > -1) {
+	console.log();
 }
 
+ // if(userChoice.length > 0) {
+	// for (var i=0; i <= 9; i++){
+	// 	wordArray[i] = userChoice;
+	// }
 
-document.onKey=hitAnyKey;
+	// }
+
+	
 
 
-//the computer make choice of a word
-//display it like _ _ _ _
-//user make a choice of a letter
-//compare the choice and determine if the letter is present in the word
-//display the result
+
+//reset condition
+		count = count - 1;
+		document.querySelector('#count').innerText = count;
+
+		if(count == -1) {
+ 			//alert('I know you can do it!');
+			count = 9;	
+		document.querySelector('#count').innerText = count;
+			wins = 0;
+		document.querySelector('#wins').innerText = wins;
+		//document.querySelector('userChoice').innerText = '&nbsp';
+
+			userChoice = "&nbsp;"; //make it blank to the begigning of the game
+		// document.getElementById('userChoice').innerHTML = "alert(hello);";
+
+		// computerOption = words[Math.floor(Math.random()*words.length)];	
+		// document.querySelector('#word').innerText = computerOption;	
+		} 
+
+
+
+
+
+
+
+		if(userChoice == computerOption){
+			winsCount++
+			document.getElementById('wins').innerHTML = winsCount;
+		} 
+
+
+
+
+
+	
+	}
+
+	document.onkeyup = startGame;
+
